a816635e8fad99f901bf998eb676b018cdfd91c7
didymu5 <wu.thomas@gmail.com>
2018-06-30 20:09:58 -0700

    removed config

--- fb.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: "AIzaSyCM_ypBKQTPInvaWZCxD5Yzh8pD_p8XHEQ",
-  authDomain: "voluntario-cbffc.firebaseapp.com",
-  databaseURL: "https://voluntario-cbffc.firebaseio.com",
-  projectId: "voluntario-cbffc",
-  storageBucket: "",
-  messagingSenderId: "171813482303"
-};
-export default config;
