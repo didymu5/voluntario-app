18097fa9bf577ef299e56ab7878bdf61125e4aaa
didymu5 <wu.thomas@gmail.com>
2018-06-30 19:50:22 -0700

    firebase added

---
+++ fb.config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyCM_ypBKQTPInvaWZCxD5Yzh8pD_p8XHEQ",
+  authDomain: "voluntario-cbffc.firebaseapp.com",
+  databaseURL: "https://voluntario-cbffc.firebaseio.com",
+  projectId: "voluntario-cbffc",
+  storageBucket: "",
+  messagingSenderId: "171813482303"
+};
+export default config;
