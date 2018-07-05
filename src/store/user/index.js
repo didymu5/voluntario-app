aba61275d90ee0d62d8ce6d5f28fcbd991670cb6
didymu5 <wu.thomas@gmail.com>
2018-07-05 14:51:09 -0700

    tracking spaces, thats not so bad is it?

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -13,31 +13,29 @@ export default {
     signUserInGoogle({ commit }) {
       commit("setLoading", true);
       commit("clearError");
-      firebase
-	.auth()
-	.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-	.then(user => {
-	  commit("setLoading", false);
-	  const newUser = {
-	    id: user.uid,
-	    name: user.displayName,
-	    email: user.email,
-	    photoUrl: user.photoURL
-	  };
-	  commit("setUser", newUser);
-	})
-	.catch(error => {
-	  commit("setLoading", false);
-	  commit("setError", error);
-	  console.log(error);
-	});
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+        .then(user => {
+          commit("setLoading", false);
+          const newUser = {
+            id: user.uid,
+            name: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL
+          };
+          commit("setUser", newUser);
+        })
+        .catch(error => {
+          commit("setLoading", false);
+          commit("setError", error);
+          console.log(error);
+        });
     },
     autoSignIn({ commit }, payload) {
       commit("setUser", {
-	id: payload.uid,
-	name: payload.displayName,
-	email: payload.email,
-	photoUrl: payload.photoURL
+        id: payload.uid,
+        name: payload.displayName,
+        email: payload.email,
+        photoUrl: payload.photoURL
       });
       commit("setLoading", false);
     },
