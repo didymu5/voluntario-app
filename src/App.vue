a779602c07fd25c7b15ca7a86b06ced2f102d383
didymu5 <wu.thomas@gmail.com>
2018-07-06 13:13:47 -0700

    Spacing stuff

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       enable-resize-watcher 
       fixed app>
       <v-list>
-	<v-list-tile value="true" v-for="(item, i) in items" :key="i" :href="item.href">
+        <v-list-tile value="true" v-for="(item, i) in items" :key="i" :href="item.href">
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
@@ -23,9 +23,9 @@
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
        <v-spacer></v-spacer>
-	<div v-if="isLoggedin" class="user-info-space">
-	  {{user.name}}
-	</div>
+        <div v-if="isLoggedin" class="user-info-space">
+          {{user.name}}
+        </div>
     </v-toolbar>
     <v-content>
         <router-view/>
@@ -52,14 +52,14 @@ export default {
       fixed: false,
       items: [
         {
-	  icon: "event_note",
-	  title: "Events",
-	  href: "/events"
-	},
-	{
-	  icon: "home",
-	  title: "Home",
-	  href: "/"
+          icon: "event_note",
+          title: "Events",
+          href: "/events"
+        },
+        {
+          icon: "home",
+          title: "Home",
+          href: "/"
         }
       ],
       miniVariant: false,
--- src/main.js
+++ src/main.js
@@ -4,10 +4,37 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import config from '../fb.config'
+import firebase from 'firebase/app'
+import store from './store/index'
 
 Vue.config.productionTip = false
+firebase.initializeApp(config)
 
 new Vue({
   router,
+  store,
+  created() {
+    this.$store.dispatch('setLoading', false)
+    if(this.$store.getters.user) {
+      console.log(this.$store.getters.user.displayName)
+    }
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+	this.$store.dispatch('autoSignIn', user)
+      }
+    })
+  },
+  computed: {
+    getUser() {
+      return this.$store.getters.user
+    }
+  },
+  method: {
+    login(user) {
+      this.$store.dispatch('autoSignIn', user)
+      this.$store.dispatch('hello', 'hi there')
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
